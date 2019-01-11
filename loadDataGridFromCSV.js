d57d9f7bf8dd8e80ee5523bbf22e2525e34455e9
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-01-11 15:41:21 +0000

    add dynamic file read

--- loadDataGridFromCSV.js
+++ loadDataGridFromCSV.js
@@ -1,27 +1,84 @@
 const worker = perspective.worker(); 
 
+const inputElement = document.getElementById("input"); 
+
 (function() {
 
-    document.addEventListener('DOMContentLoaded', function() {
+    // document.addEventListener('DOMContentLoaded', function() {
 
-        const gridDiv = document.querySelector('#myGrid');
+    //     const gridDiv = document.querySelector('#myGrid');
 
-        const gridOptions = {
-            columnDefs: []
-        };
+    //     const gridOptions = {
+    //         columnDefs: []
+    //     };
 
         
-        jsonLoad( function(colDefs, data) {
-            gridOptions.columnDefs = colDefs;            
-            new agGrid.Grid(gridDiv, gridOptions);
-            gridOptions.api.setRowData(data);
-        });
-    });
+    //     jsonLoad( function(colDefs, data) {
+    //         gridOptions.columnDefs = colDefs;            
+    //         new agGrid.Grid(gridDiv, gridOptions);
+    //         gridOptions.api.setRowData(data);
+    //     });
+    // });
+
+    inputElement.addEventListener("change", handleFiles, false);
+    function handleFiles() {
+      const fileList = this.files; 
+    
+      [...fileList].forEach(uploadFile);  
+    }
 
 })();
 
+function uploadFile(file) {
+    console.log ("Upload file: " + file.name);
 
-function jsonLoad(callback) {
+    const gridDiv = document.querySelector('#myGrid');
+
+    const gridOptions = {
+        columnDefs: []
+    };
+
+    uploadCSVFile( file, function(colDefs, data) {
+        gridOptions.columnDefs = colDefs;            
+        new agGrid.Grid(gridDiv, gridOptions);
+        gridOptions.api.setRowData(data);
+    });
+}
+
+function csvLoadFromFile(file, callback) {
+    const table = worker.table(file);
+
+    table.schema().then(schema => {
+        console.log(schema);
+        const colDefs = getColumnDefinitionsFromSchema(schema);
+        console.log(colDefs);
+        
+        table.view().to_json().then(data => {
+            callback(colDefs, data);
+        });
+    })         
+}
+
+function uploadCSVFile(file, callback) {
+    let reader = new FileReader();
+    reader.onload = function(fileLoadedEvent) {
+        let txt = fileLoadedEvent.target.result;
+        const table = worker.table(txt);
+        
+        table.schema().then(schema => {
+            console.log(schema);
+            const colDefs = getColumnDefinitionsFromSchema(schema);
+            console.log(colDefs);
+            
+            table.view().to_json().then(data => {
+                callback(colDefs, data);
+            });
+        });
+    };
+    reader.readAsText(file);
+}
+
+function csvLoad(callback) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '../data/FL_insurance_sample.csv'); // by default async
     xhr.responseType = 'text'; // in which format you expect the response to be
